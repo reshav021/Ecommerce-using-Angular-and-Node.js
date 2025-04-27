1278472a0632dcb3b2eb2be50d2cdd757f666dce
Reshav <reshav021@gmail.com>
2025-04-27 23:15:53 +0530

    added more components for admin and profile

---
+++ ecommerce-frontend/src/app/components/profile/profile-information/profile-information.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-information',
+  standalone: false,
+  templateUrl: './profile-information.component.html',
+  styleUrl: './profile-information.component.css'
+})
+export class ProfileInformationComponent {
+
+}
