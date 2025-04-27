ab2c41ad44e839a4d1b1ba538842dc124fd0e3d4
Reshav <reshav021@gmail.com>
2025-04-27 23:04:10 +0530

    Added components

---
+++ ecommerce-frontend/src/app/components/orders/checkout-page/checkout-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-checkout-page',
+  standalone: false,
+  templateUrl: './checkout-page.component.html',
+  styleUrl: './checkout-page.component.css'
+})
+export class CheckoutPageComponent {
+
+}
